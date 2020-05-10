7ebc4626c8b349569ad3519d7c51eb4a63a563e5
BenjaminPatch <benpatch@hotmail.com>
2020-05-10 20:02:13 +1000

    Hello World N Times

---
+++ haskell/hackerRank/fpPath/Hello World N Times/Solution.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE FlexibleInstances, UndecidableInstances, DuplicateRecordFields #-}
+
+module Main where
+
+import Control.Monad
+import Data.Array
+import Data.Bits
+import Data.List
+import Data.List.Split
+import Data.Set
+import Debug.Trace
+import System.Environment
+import System.IO
+import System.IO.Unsafe
+
+-- Below has two solutions, one commented out.
+-- Print "Hello World" on a new line 'n' times.
+main :: IO()
+main = do
+    n <- readLn :: IO Int
+
+    -- create a [IO ()] with replicate, then evaluates each 
+    -- of these monadic structures, ignoring the results.
+    sequence_ $ replicate n $ putStrLn "Hello World"
+
+    -- carry out putStrLn n times, ignoring the results
+    replicateM_ n $ putStrLn "Hello world"
+
